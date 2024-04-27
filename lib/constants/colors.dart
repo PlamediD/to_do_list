fca99536d9f26e6682cebd5c687fa45ff7545cd2
pdiakubama <pdiakubama@seattleu.edu>
2024-04-27 06:51:27 -0700

    Initial commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed = Color(0xFF4CAF50);
+const Color tdBlue = Color(0xFF2196F3);
+
+const Color tdBlack = Color(0xFF333333);
+const Color tdGrey = Color(0xFF757575);
+
+const Color tdBGColor = Color(0xFFEEEFF5);
+
+/*
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF4CAF50); // Green
+const Color secondaryColor = Color(0xFF2196F3); // Blue
+
+const Color textColor = Color(0xFF333333); // Dark grey
+const Color hintColor = Color(0xFF757575); // Light grey
+
+const Color backgroundColor = Color(0xFFF5F5F5); // Light grey
+
+ */
---
+++ lib/model/todo.dart
@@ -0,0 +1,11 @@
+class ToDo{
+  String id;
+  String toDoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.toDoText,
+    this.isDone = false,
+  });
+}
