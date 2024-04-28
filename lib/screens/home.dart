e79472b1564b4be53148aba8daf28110bd690fc1
pdiakubama <pdiakubama@seattleu.edu>
2024-04-28 11:59:44 -0700

    Started working the home page

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,9 +4,33 @@ import '../model/todo.dart';
 import '../constants/colors.dart';
 import '../widgets/todo_item.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
 
   @override
-  State<Home> createState() => _HomeState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+                Icons.menu,
+              color: tdBlack,
+              size:30,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                child: Image.asset('images/avatar.jpg'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(child: Text('This is ')),
+    );
+  }
 }
