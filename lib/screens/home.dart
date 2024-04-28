9ff63046b589540d1a16281eed2a847bc2b7ac06
pdiakubama <pdiakubama@seattleu.edu>
2024-04-28 11:09:38 -0700

    contuined working on the app. Finished the model and color palette

---
+++ lib/screens/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../model/todo.dart';
+import '../constants/colors.dart';
+import '../widgets/todo_item.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
