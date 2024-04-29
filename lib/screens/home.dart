080d1803d023f4f5d1f7afb7cc49d549070b8d1c
pdiakubama <pdiakubama@seattleu.edu>
2024-04-29 13:05:25 -0700

    Started working the search bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../model/todo.dart';
 import '../constants/colors.dart';
-import '../widgets/todo_item.dart';
+
 
 class Home extends StatelessWidget {
   Home({Key? key}) : super(key: key);
@@ -10,27 +8,98 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Icon(
-                Icons.menu,
-              color: tdBlack,
-              size:30,
-            ),
-            Container(
-              height: 40,
-              width: 40,
-              child: ClipRRect(
-                child: Image.asset('images/avatar.jpg'),
+      backgroundColor: tdBGColor,
+      appBar: buildAppBar(),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
+          child: Column(
+            children: [
+              SizedBox(height: 20), // Add some space at the top
+              searchBox(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(
+                        top: 50,
+                        bottom:20,
+                      ),
+                      child: Text(
+                        'All ToDos',
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+              // Add more widgets here as needed
+            ],
+          ),
         ),
       ),
-      body: Container(child: Text('This is ')),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              //borderRadius: BorderRadius.circular(20),
+              child: Image.asset('images/avatar.jpg'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class searchBox extends StatelessWidget {
+  const searchBox({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(0),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minHeight: 20,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
+        ),
+      ),
     );
   }
 }
